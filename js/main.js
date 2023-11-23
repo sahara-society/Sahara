64d031678553b805bf3cc6c1868f7024d78e4e25
Parampreet Singh <rubercuber.30@gmail.com>
2023-11-23 14:25:13 +0530

    Fixed current-page highlight

--- js/main.js
+++ js/main.js
@@ -1,4 +1,13 @@
+"use strict";
+
+if (window.scrollY > 0) {
+    document.querySelector("#navigation").classList.toggle("on-scroll", true);
+    document.querySelector("#logo img").classList.toggle("shrink-on-scroll", true);
+    document.querySelector("#current-page").classList.toggle("shrink-nav", true);
+}
+
 window.addEventListener("scroll", () =>{
-    document.querySelector("#navigation").classList.toggle("on-scroll",window.scrollY > 0)
-    document.querySelector("#logo img").classList.toggle("shrink-on-scroll", window.scrollY > 0)
+    document.querySelector("#navigation").classList.toggle("on-scroll",window.scrollY > 0);
+    document.querySelector("#logo img").classList.toggle("shrink-on-scroll", window.scrollY > 0);
+    document.querySelector("#current-page").classList.toggle("shrink-nav", window.scrollY > 0);
 })
