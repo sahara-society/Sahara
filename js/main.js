000372154aacd536c21c037fca3a460988b8e8ed
Parampreet Singh <rubercuber.30@gmail.com>
2023-11-24 19:25:24 +0530

    Fixed activities section and started slider

--- js/main.js
+++ js/main.js
@@ -11,3 +11,24 @@ window.addEventListener("scroll", () =>{
     document.querySelector("#logo img").classList.toggle("shrink-on-scroll", window.scrollY > 0);
     document.querySelector("#current-page").classList.toggle("shrink-nav", window.scrollY > 0);
 })
+
+// ================================ About ===============================
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+    let i;
+    let slides = document.getElementsByClassName("slides");
+    let dots = document.getElementsByClassName("dot");
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";  
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) {slideIndex = 1}    
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex-1].style.display = "block";  
+    dots[slideIndex-1].className += " active";
+    setTimeout(showSlides, 5000)
+}
