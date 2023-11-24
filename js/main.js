c7c5ad5011574f41b0dc1c508e23255d368c0580
Parampreet Singh <rubercuber.30@gmail.com>
2023-11-24 21:21:41 +0530

    About:activities done, outreach pending
    Images selection is left in activities, cards need to be fixed in outreach

--- js/main.js
+++ js/main.js
@@ -25,10 +25,6 @@ function showSlides() {
     }
     slideIndex++;
     if (slideIndex > slides.length) {slideIndex = 1}    
-    for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(" active", "");
-    }
     slides[slideIndex-1].style.display = "block";  
-    dots[slideIndex-1].className += " active";
     setTimeout(showSlides, 5000)
 }
